94193aba7ee25992af92246a8854689264bfdba4
David Gómez Chiu <david.gomez.chiu@gmail.com>
2020-05-19 20:02:08 -0500

    Inicio de emergency info

--- lib/pages/pacient/emergency_info_page.dart
+++ lib/pages/pacient/emergency_info_page.dart
@@ -10,8 +10,31 @@ class _EmergencyInfoPageState extends State<EmergencyInfoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Emergency Info Page'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.all(15.0),
+              children: <Widget>[
+
+              ],
+            ),
+          ),
+          ButtonTheme(
+            minWidth: 220,
+            child: RaisedButton(
+              textColor: Colors.white,
+              shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+              color: Color.fromRGBO(104, 202, 184, 1),
+              padding: const EdgeInsets.all(5.0),
+              onPressed: () {
+                
+              },
+              child: const Text('Cambiar contraseña')
+            )
+          ),
+        ],
       ),
     );
   }
