50167a6e46adfbeba318f7ad69c71c71108780fd
diogo1b <xlane667@gmail.com>
2020-05-19 19:02:18 -0500

    Se dan de alta los servicios para los diversos roles

---
+++ lib/services/paramedic.dart
@@ -0,0 +1,8 @@
+import 'dart:convert';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:healthchainflutter/globals.dart' as globals;
+import 'package:http/http.dart' as http;
+
+class ParamedicService {
+
+}
