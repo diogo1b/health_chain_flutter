e4375bf3e79b6ca1e6be5fb089cc739dde7f24fc
diogo1b <xlane667@gmail.com>
2020-05-20 13:15:05 -0500

    Se agrega el filtro de paramédico

--- lib/services/paramedic_service.dart
+++ lib/services/paramedic_service.dart
@@ -4,30 +4,30 @@ import 'package:healthchainflutter/globals.dart' as globals;
 import 'package:healthchainflutter/models/Emergency_Info.dart';
 import 'package:http/http.dart' as http;
 
-class ParamedicService {
-  Future<Emergency_Info> getUserEmergencyInfo(String email) async {
+  class ParamedicService {
+    Future<Emergency_Info> getUserEmergencyInfo(String email) async {
 
-    var body = json.encode(
-      {
-        "email": email,
-      });
+      var body = json.encode(
+        {
+          "email": email,
+        });
 
-  Map<String, String> headers = {
-    'Content-type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': globals.user.token
-  };
+    Map<String, String> headers = {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': globals.user.token
+    };
 
-  final response =
-      await http.post(
-  globals.API_URL + "/paramedic/show_user", body: body, headers: headers);
-  final responseJson = json.decode(response.body);
+    final response = await http.post(globals.API_URL + "/paramedic/show_user", body: body, headers: headers);
+    final responseJson = json.decode(response.body);
 
-  print(responseJson);
-  print(responseJson['user']['_emergency_info']);
+    print(responseJson);
 
-  var emergency_info = responseJson['user']['_emergency_info'];
-  return Emergency_Info(emergency_info['name'], emergency_info['age'], emergency_info['blood_type'], emergency_info['weight'], emergency_info['height'], emergency_info['e_contact_name'], emergency_info['e_contact_phone'], emergency_info['e_relationship'], emergency_info['medical_condition'], emergency_info['allergies'], emergency_info['preferred_hospital']);
-
+    if(responseJson['succes'] == "false") {
+      Emergency_Info(null, null, null, null, null, null, null, null, null, null, null);
+    } else {
+      var emergency_info = responseJson['user']['_emergency_info'];
+      return Emergency_Info(emergency_info['name'], emergency_info['age'], emergency_info['blood_type'], emergency_info['weight'], emergency_info['height'], emergency_info['e_contact_name'], emergency_info['e_contact_phone'], emergency_info['e_relationship'], emergency_info['medical_condition'], emergency_info['allergies'], emergency_info['preferred_hospital']);
+    }
   }
 }
